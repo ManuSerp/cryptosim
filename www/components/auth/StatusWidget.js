0dffe2f6a9db87458b35164cba087ac8b868f499
Serp <manu.serpette74@gmail.com>
2022-03-02 17:38:29 +0100

    t

--- www/components/auth/StatusWidget.js
+++ www/components/auth/StatusWidget.js
@@ -20,7 +20,7 @@ function StatusWidget() {
 
       {session && (
         <a>
-          <button onClick={logoutHandler}>Logout</button> {session}
+          <button onClick={logoutHandler}>Logout</button> {session.user.pseudo}
         </a>
       )}
     </>
