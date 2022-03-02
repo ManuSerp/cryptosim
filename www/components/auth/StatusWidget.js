f25f5b8c3319676892d52865d59b5d7a6ada3267
Serp <manu.serpette74@gmail.com>
2022-03-02 17:25:24 +0100

    pseudo auto

--- www/components/auth/StatusWidget.js
+++ www/components/auth/StatusWidget.js
@@ -20,7 +20,7 @@ function StatusWidget() {
 
       {session && (
         <a>
-          <button onClick={logoutHandler}>Logout</button> pseudo{" "}
+          <button onClick={logoutHandler}>Logout</button> {session.pseudo}
         </a>
       )}
     </>
