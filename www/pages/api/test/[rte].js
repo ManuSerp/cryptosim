3a74d280576553b6948815e803463a02a9c4f93b
Serp <manu.serpette74@gmail.com>
2022-03-03 09:04:36 +0100

    query

--- www/pages/api/test/[rte].js
+++ www/pages/api/test/[rte].js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   });
 
   if (!rest) {
-    res.status(200).json({ error: "no results" });
+    res.status(200).json({ error: "no results", query: req.query });
   } else {
     res.status(200).json(rest);
   }
