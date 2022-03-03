53961bd5f1ddc1b24413b7e25418074b5b81f31d
Serp <manu.serpette74@gmail.com>
2022-03-03 08:54:48 +0100

    api

---
+++ www/pages/api/test/[rte].js
@@ -0,0 +1,22 @@
+import { getSession } from "next-auth/react";
+import clientPromise from "../../../lib/mongodb";
+
+export default function handler(req, res) {
+  const client = await clientPromise;
+  const users = await client.db().collection("user_id");
+  const rest = await users.findOne({
+    psd: req.query,
+  });
+
+  let sess;
+
+  getSession().then((session) => {
+    sess = session;
+  });
+
+  if (!rest) {
+    res.status(200).json({ error: "no results" });
+  } else {
+    res.status(200).json(rest);
+  }
+}
--- www/pages/api/test/session.js
+++
@@ -1,5 +0,0 @@
-import { getSession } from "next-auth/react";
-
-export default function handler(req, res) {
-  res.status(200).json(req);
-}
