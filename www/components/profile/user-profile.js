0595ebb61fe078bd0b00ea9d0da6387cd29bcdba
Serp <manu.serpette74@gmail.com>
2022-03-05 21:03:48 +0100

    userProfile

--- www/components/profile/user-profile.js
+++ www/components/profile/user-profile.js
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 function UserProfile() {
   const { data: session, status } = useSession();
@@ -10,6 +11,13 @@ function UserProfile() {
       {!session && !loading && <a>ERROR</a>}
 
       {session && <a>{session.user.name}</a>}
+
+      {session.user.name == "admin" && (
+        <a>
+          {" "}
+          <Link href="/admin/root">admin page</Link>
+        </a>
+      )}
     </>
   );
 }
