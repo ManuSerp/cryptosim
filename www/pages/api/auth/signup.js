639c1c4d759f086f4e3c4f5bb8dbde2a6cad1c91
Nathan <nathan.galmiche@sfr.fr>
2022-03-06 15:36:10 +0100

    correc_closing

--- www/pages/api/auth/signup.js
+++ www/pages/api/auth/signup.js
@@ -20,7 +20,6 @@ async function handler(req, res) {
         //Send error response if duplicate user is found
         if (checkExisting) {
             res.status(422).json({ message: 'User already exists' });
-            client.close();
             return;
         }
         //Hash password
@@ -30,8 +29,6 @@ async function handler(req, res) {
         });
         //Send success response
         res.status(201).json({ message: 'User created', ...status });
-        //Close DB connection
-        client.close();
     } else {
         //Response for other than POST method
         res.status(500).json({ message: 'Route not valid' });
