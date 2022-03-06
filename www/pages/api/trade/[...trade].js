f995ff98752c1df1b5349a0ee4543bcd3f5ca528
Serp <manu.serpette74@gmail.com>
2022-03-06 12:25:30 +0100

    patch err

--- www/pages/api/trade/[...trade].js
+++ www/pages/api/trade/[...trade].js
@@ -59,7 +59,7 @@ export default async function handler(req, res) {
     //
 
     res.status(200).json(pipe);
+  } else {
+    res.status(200).json({ error: "not coins enough" });
   }
-
-  res.status(200).json({ error: "not coins enough" });
 }
