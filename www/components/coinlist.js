e1f0d33bf87b57eed0392ad87cf37207ed50a6d6
DatBen <benjamindumont@live.com>
2022-03-07 17:12:06 +0100

    init call api

--- www/components/coinlist.js
+++ www/components/coinlist.js
@@ -1,15 +1,30 @@
 import CoinPrice from "./coinprice";
+import useSWR from "swr";
+
+const fetcherCoin = async (url) => {
+  const response = await fetch(url).then((response) => response.json());
+  return response;
+};
 
 export default function CoinList() {
-  const coinlist = [
-    { name: "BTC", value: 100 },
-    { name: "ETH", value: 1000 },
-  ];
+  const coinlist = [{ name: "bitcoin", value: 0 }];
+
+  const { data, error } = useSWR("/api/crypto/bitcoin", fetcherCoin, {
+    refreshInterval: 10000,
+  });
+
+  if (error) {
+    return <div>failed to load </div>;
+  }
+
+  if (!data) {
+    return <div>loading...</div>;
+  }
   return (
     <div className="CoinList">
       <p>Stock Price :</p>
       {coinlist.map(({ name, value }, i) => (
-        <CoinPrice key={i} name={name} value={value} />
+        <CoinPrice key={i} name={name} value={data.eur} />
       ))}
     </div>
   );
