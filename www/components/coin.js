b8bf5cd42803b54a3aca4357384bdcf5083b9380
Pierre Blachere <pierre.blachere@outlook.com>
2022-03-08 18:13:13 +0100

    changements au wallet

--- www/components/coin.js
+++
@@ -1,8 +0,0 @@
-export default function CoinCard({ coin, amount }) {
-    return (
-      <div className="coin-card">
-        {coin}: {amount}
-      </div>
-    );
-  }
-  
--- www/components/wallet.js
+++ www/components/wallet.js
@@ -1,5 +1,41 @@
-import CoinCard from "./coin";
-import coin from "./coin";
+import CoinCard from "./walletcontents";
+import useSWR from "swr";
+
+
+const coinlistjson = {
+    btc: "bitcoin",
+    eth: "ethereum",
+    usd: "uniswap-state-dollar",
+    usdt: "tether",
+    bnb: "binancecoin",
+    xrp: "ripple",
+  };
+  const coinlist = [];
+  for (var c in coinlistjson) {
+    coinlist.push(coinlistjson[c]);
+  }
+  
+
+
+const fetcher = async (url) => {
+    const response = await fetch(url).then((response) => response.json());
+    return response;
+  };
+
+const showWalletContent = (i, name) => {
+    const { data, error } = useSWR("/api/db/" + name, fetcher, {
+      refreshInterval: 10000,
+    });
+  
+    if (error) {
+      return <div>failed to load </div>;
+    }
+  
+    if (!data) {
+      return <div>loading...</div>;
+    }
+    return <CoinPrice key={i} name={name} value={data.usd} />;
+  };
 
 export default function Wallet() {
     const WalletInfo=[{ coin: "dogecoin", amount: 1000 },{coin:"bit", amount:1},{coin:"sha",amount:1}
