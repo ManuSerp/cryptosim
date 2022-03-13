32b2615475a30c7923e949c5e05819566aa4dde7
Serp <manu.serpette74@gmail.com>
2022-03-13 19:15:42 +0100

    patch trade issue on usd

--- www/pages/api/trade/[...trade].js
+++ www/pages/api/trade/[...trade].js
@@ -36,19 +36,27 @@ export default async function handler(req, res) {
     return res.status(501).end();
   }
   //check price
-
-  const price = await fetch(
-    process.env.ABS_URL + "/api/crypto/" + req.query.trade[0]
-  );
+  if (req.query.trade[1] === "usd") {
+    const price = await fetch(process.env.ABS_URL + "/api/crypto/usd");
+  } else {
+    const price = await fetch(
+      process.env.ABS_URL + "/api/crypto/" + req.query.trade[0]
+    );
+  }
 
   if (!price) {
     res.status(502).end();
   }
 
   const json = await price.json();
+
   let from = req.query.trade[1];
 
-  let value_to_pay = json[from] * req.query.trade[2];
+  if (req.query.trade[1] === "usd") {
+    let value_to_pay = req.query.trade[2] / json[req.query.trade[0]];
+  } else {
+    let value_to_pay = json[from] * req.query.trade[2];
+  }
 
   if (result[from] && result[from] >= value_to_pay) {
     //upadte
