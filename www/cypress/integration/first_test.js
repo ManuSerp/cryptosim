4bebcb0c95edb737ec588b088d424b00e4b869f2
DatBen <benjamindumont@live.com>
2022-04-06 10:29:48 +0200

    update

--- www/cypress/integration/first_test.js
+++ www/cypress/integration/first_test.js
@@ -1,9 +1,18 @@
-describe("Test Login", () => {
-  it("Test Login", () => {
-    cy.visit("https://cryptosim.vercel.app/");
+describe("Test", () => {
+  const url = "http://localhost:3000/";
+  it("Test", () => {
+    cy.visit(url);
     cy.get(".but-Login").click();
-    cy.url().should("include", "/auth");
-    cy.get("#pseudo").type("ben").should("have.value", "ben");
+    cy.url().should("eq", url + "auth");
+    cy.get("#pseudo").type("test").should("have.value", "test");
     cy.get("#password").type("test").should("have.value", "test");
+    cy.get(".auth-5-but1").click();
+    cy.url().should("eq", url);
+    cy.intercept({
+      method: "GET",
+      url: "/api/trade/usdt/eur/1",
+    }).as("tradeCheck");
+    cy.get("#CoinPrice-usdt #quantite").type("1").should("have.value", "1");
+    cy.get("#CoinPrice-usdt .buy").click();
   });
 });
